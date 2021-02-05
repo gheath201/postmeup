b27374d0a6ba6c1298be11ec67e20fbee11c42af
Gavin <Gheath201@gmail.com>
2021-02-05 00:23:44 -0500

    addding posts model

--- postsMain/models.py
+++ postsMain/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class post(models.Model):
+	author = models.CharField(max_length=40)
+	time_posted = models.DateTimeField('Time Posted')
+	post = models.CharField(max_length=1000)
+
+	
