054e38806c33f56830bd37c0aa8eb579e86c4d50
Gavin <Gheath201@gmail.com>
2021-02-04 23:38:25 -0500

    adding djongo

--- postMeUp/settings.py
+++ postMeUp/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'postsMain',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,11 @@ WSGI_APPLICATION = 'postMeUp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'djongo',
+        'NAME': 'postsMain',
+        'CLIENT' : {
+            'host' : '127.0.0.1',
+        }
     }
 }
 
