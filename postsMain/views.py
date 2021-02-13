bc4e3d8f72540a13f7c5d4d1e7b8c67b12faf8d6
Gavin <Gheath201@gmail.com>
2021-02-12 23:25:13 -0500

    adding models and views

--- postsMain/views.py
+++ postsMain/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from postsMain.models import post
 
-# Create your views here.
+def post(request, pk):
+	post_obj = post.objects.get(pk=pk)
+	context = {
+	'post':post_obj
+	}
+
+	return render(request, "postsMain.html", context)
